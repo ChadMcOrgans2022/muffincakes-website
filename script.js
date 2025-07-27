cef345bf765594c544d9fc57484554bf24bc151b
ChadMcOrgans2022 <121216038+ChadMcOrgans2022@users.noreply.github.com>
2025-07-27 17:21:36 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ let x, y
 let position
 let width = window.innerWidth - 100;
 let height = window.innerHeight - 100;
+const text = document.getElementById("Text")
+text.innerHTML = "This button is RACIST! Click it to punish it for it's racism! It will try to run, but don't let it get away!<br>It's really fast, so be careful!"
+const restartButton = document.getElementById("RestartButton")
+restartButton.style.display = "none"
 
 
 function getRandom() {
@@ -21,6 +25,7 @@ function getPosition() {
     y = position[1]
     console.log(x, y)
     box.style.position = "relative"
+    
     box.style.transform = `translate(${x}px, ${y}px)`
 }
 
@@ -29,4 +34,7 @@ function endGame() {
     box.style.display = "none"
     const button = document.getElementById("Button1")
     button.style.display = "none"
+    text.innerHTML = "Hooray! You murdered the Racist Button™<br>It will never be racist again!<br>Click anywhere to play again!"
+    restartButton.style.display = "block"
+
 }
